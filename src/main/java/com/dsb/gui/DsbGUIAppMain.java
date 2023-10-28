386464571d8004490e72cd3965935113e3cce4da
-Lunat1c- <arturgrabowski100@onet.pl>
2023-10-28 23:26:50 +0200

    Added console package

--- src/main/java/com/dsb/gui/DsbGUIAppMain.java
+++ src/main/java/com/dsb/gui/DsbGUIAppMain.java
@@ -12,7 +12,7 @@ public class DsbGUIAppMain extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(DsbGUIAppMain.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("DiskSpaceBuddy");
         stage.setScene(scene);
         stage.show();
     }
