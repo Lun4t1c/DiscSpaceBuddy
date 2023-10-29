43e6f566f22d656f0f89975a62b74bc78c3e4fb2
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2023-10-29 12:24:01 +0100

    Terminal/console GUI/UI with UX update...

--- src/main/java/com/dsb/console/DsbConsoleMain.java
+++ src/main/java/com/dsb/console/DsbConsoleMain.java
@@ -2,18 +2,53 @@ package com.dsb.console;
 
 import com.dsb.core.DsbScanner;
 
+import java.util.Scanner;
+
 public class DsbConsoleMain {
     public static void main(String[] args) {
+        DsbScanner dsbScanner = new DsbScanner();
+        Scanner scanner = new Scanner(System.in);
+
+        char choice;
+
+        boolean b = true;
+
         printSplashScreen();
 
-        new DsbScanner();
+        while (b) {
+            printMenu();
+            choice = scanner.next().charAt(0);
+            switch (choice) {
+                case 's':
+                    dsbScanner.performScan();
+                    break;
+                case 'q':
+                    b = false;
+                    break;
+                default:
+                    System.err.println("Undefined command, maggot :D...");
+                    break;
+            }
+        }
     }
 
     public static void printSplashScreen() {
         System.out.println(
-                        """
+                """
                         ==============================
                         "        DiskSpaceBuddy      "
+                        ==============================
+                        """
+        );
+    }
+
+    public static void printMenu() {
+        System.out.println(
+                """
+                        ==============================
+                        "      Press 's' to scan     "
+                        "      Press 'q' to exit     "
+                        "      Press '?' for help    "
                         ==============================
                         """
         );
