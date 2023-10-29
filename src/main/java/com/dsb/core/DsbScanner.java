bc30b575480fcef7ed7cf417749dee086cd01672
-Lunat1c- <arturgrabowski100@onet.pl>
2023-10-29 11:23:16 +0100

    Asynchronous directories scanning

--- src/main/java/com/dsb/core/DsbScanner.java
+++ src/main/java/com/dsb/core/DsbScanner.java
@@ -2,11 +2,14 @@ package com.dsb.core;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class DsbScanner {
-    public List<Path> Directories = new LinkedList<>();
+    public List<Path> Directories = new ArrayList<>();
+    List<CompletableFuture<Void>> taskList = new ArrayList<>();
 
     public DsbScanner() {
         performScan();
@@ -17,22 +20,41 @@ public class DsbScanner {
 
         for (Path disc : filesystem.getRootDirectories()) {
             System.out.println("Found disc: " + disc + " - scanning...");
-            ScanDirectory(disc.toString());
+            taskList.add(CompletableFuture.runAsync(() -> ScanDirectory(disc.toString())));
+        }
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(taskList.toArray(new CompletableFuture[0]));
+
+        try {
+            allOf.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
 
-        System.out.println("Scanning done.");
+        System.out.println("\nScanning done. (found " + Directories.size() + " directories.)");
     }
 
     private void ScanDirectory(String path) {
         System.out.println("Scanning directory " + path);
+        //List<CompletableFuture<Void>> taskList = new ArrayList<>();
 
         try (DirectoryStream<Path> subfolders = java.nio.file.Files.newDirectoryStream(Paths.get(path))) {
             for (Path subfolder : subfolders) {
                 if (java.nio.file.Files.isDirectory(subfolder)) {
                     System.out.println("Subfolder: " + subfolder);
                     Directories.add(subfolder);
-                    //ScanDirectory(subfolder.toString());
+                    /*
+                    taskList.add(CompletableFuture.runAsync(() -> {
+                        ScanDirectory(subfolder.toString());
+                    }));
+                     */
                 }
+            }
+
+            CompletableFuture<Void> allOf = CompletableFuture.allOf(taskList.toArray(new CompletableFuture[0]));
+            try {
+                allOf.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
