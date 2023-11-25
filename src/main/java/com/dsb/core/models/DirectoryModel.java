667948930a827a5f100349642a9373dd3de51ff2
Lunat1c <artur.lunat1c.grabowski@gmail.com>
2023-11-25 15:46:45 +0100

    Subfolders and files pt1

--- src/main/java/com/dsb/core/models/DirectoryModel.java
+++ src/main/java/com/dsb/core/models/DirectoryModel.java
@@ -1,10 +1,15 @@
 package com.dsb.core.models;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DirectoryModel {
     private final Path path;
     private long size;
+
+    private List<DirectoryModel> subDirectories = new ArrayList<>();
+    private List<FileModel> files = new ArrayList<>();
 
     public DirectoryModel(Path path, long size) {
         this.path = path;
@@ -21,5 +26,13 @@ public class DirectoryModel {
 
     public void setSize(long size) {
         this.size = size;
+    }
+
+    public void addFile(FileModel file) {
+        files.add(file);
+    }
+
+    public void addSubDirectory(DirectoryModel directory) {
+        subDirectories.add(directory);
     }
 }
